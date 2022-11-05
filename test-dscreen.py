cd9b34b15711b5a5c254c330c3ed79d7ca637217
lukas.kempf <lukas.kempf@gwf.ch>
2022-11-05 10:45:00 +0100

    fix:is_raspberrypi

--- test-dscreen.py
+++ test-dscreen.py
@@ -26,8 +26,10 @@ import traceback
 
 def is_raspberrypi():
     try:
-        with io.open('/sys/firmware/devicetree/base/model', 'r') as m:
-            if 'raspberry pi' in m.read().lower(): print("rpi"); return True
+        with open('/sys/firmware/devicetree/base/model', 'r') as m:
+            if 'raspberry pi' in m.read().lower():
+                print("rpi");
+                return True
     except Exception: pass
     return False
 if is_raspberrypi():
