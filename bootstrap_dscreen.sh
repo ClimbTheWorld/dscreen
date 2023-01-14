36bd9407692915bd4915e32e6888a86c9720202a
lukas kempf <lukaskempf@gmail.com>
2023-01-14 16:26:25 +0100

    update: bootstrap_dscreen.sh and requirements.txt

--- bootstrap_dscreen.sh
+++ bootstrap_dscreen.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 # bootstrap dscreen
-if [ $(id) -eq "0" ]; then
+if [[ "$EUID" -ne 0 ]]; then
   echo "you are not root this is required"
   exit 1
 fi
-sudo apt update -y && sudo apt upgrade -y
-sudo apt install git RPi.GPIO python3 python3-pip -y
-wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
+if [[ ! -f "first" ]]; then
+  echo "Start first part of installation"
+  sudo apt update -y && sudo apt upgrade -y
+  sudo apt install git python3-rpi.gpio python3 python3-pip -y
+  sed -i "s/#dtparam=spi=on/dtparam=spi=on/g" /boot/config.txt
+  touch first
+  reboot
+else
+  echo "Start second part of installation"
+  wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
+  tar -zxvf bcm2835-1.60.tar.gz
+  cd bcm2835-1.60
+  ./configure
+  make
+  make check
+  make install
+  cd ..
 
-git clone https://github.com/climbtheworld/dscreen
-cd dscreen
-pip install -r requirements.txt
-crontab -l > cron_dscreen
-echo "SHELL=/bin/bash
+  pip install -r requirements.txt
+  crontab -l > cron_dscreen
+  echo "SHELL=/bin/bash \
 0,24,27,30 5,6,7,8,* * * * cd /home/pi/e-paper && python test-dscreen.py 2&1 >logger" >> cron_dscreen
-crontab cron_dscreen
-rm cron_dscreen
+  crontab cron_dscreen
+  rm -f cron_dscreen
+  
+  git clone https://github.com/ClimbTheWorld/dscreen
+  cd dscreen
+  rm -f first
+fi
